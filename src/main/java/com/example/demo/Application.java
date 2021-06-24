2987c47af452a8041c3129ad2ace869e3c421f6e
vinod singh <vinod.rawat@dxc.com>
2021-06-24 12:12:05 +0530

    HIS-134 fix provided

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -9,7 +9,7 @@ public class Application {
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext run = SpringApplication.run(Application.class, args);
-		
+		//Changes for HIS-134
 		run.close();
 	}
 
